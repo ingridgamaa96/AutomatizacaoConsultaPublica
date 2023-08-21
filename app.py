dfb38492359fabdd9fb40afd6674519eeebc429b
Ingridg <49728531+ingridgamaa96@users.noreply.github.com>
2023-08-21 20:42:06 -0300

    incluindo oab e estado e botao pesquisar

--- app.py
+++ app.py
@@ -18,9 +18,30 @@ from time import sleep
 #guardar tudo no exel, separados pro processo 
 
 
+
+numero_oab=133864
+
+
+#Entrar site https://pje-consulta-publica.tjmg.jus.br/
+
 servico = Service (ChromeDriverManager().install())
 driver = webdriver.Chrome(service=servico)
 driver.get('https://pje-consulta-publica.tjmg.jus.br/')
-sleep(30)
+sleep(100)
 
 
+#digitar oab
+campo_oab = driver.find_element(By.XPATH, "input [@id='fPP:Decoration:numeroOAB']")
+campo_oab.send_keys(numero_oab)
+
+#selecionar estado 
+
+dropdown_estados = driver.find_element(By.XPATH, "//select[@id='fPP:Decoration:estadoComboOAB']")
+opcoes_estados = Select(dropdown_estados)
+opcoes_estados.select_by_visible_text('SP')
+
+#clicar e pesquisar
+
+botao_pesquisar = driver.find_element(By.XPATH, '//input[@id="fPP:searchProcessos"]')
+botao_pesquisar.click()
+sleep(10)
